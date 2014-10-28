b9f5736bac5d83f1c46a3ca6e55722e2f655b941
unknown <rafael.carrion@technit.com.mx>
2014-10-28 17:04:09 -0600

    cambios splashscreen

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+navigator.splashscreen.show();
 var images = new Array()
 function preload() {
 	for (i = 0; i < preload.arguments.length; i++) {
@@ -54,6 +55,9 @@ var app = {
     // The scope of `this` is the event. In order to call the `receivedEvent`
     // function, we must explicity call `app.receivedEvent(...);`
     onDeviceReady: function() {
+		setTimeout(function(){
+			navigator.splashscreen.hide();
+		},1000);
         app.receivedEvent('deviceready');
     },
 
