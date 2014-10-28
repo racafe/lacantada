d1e5c7a50bb306ea23732f0a6e5e047fd8900064
unknown <rafael.carrion@technit.com.mx>
2014-10-28 17:56:40 -0600

    cambios

--- js/index.js
+++ js/index.js
@@ -54,10 +54,10 @@ var app = {
     // The scope of `this` is the event. In order to call the `receivedEvent`
     // function, we must explicity call `app.receivedEvent(...);`
     onDeviceReady: function() {
+        app.receivedEvent('deviceready');
 		setTimeout(function(){
 			navigator.splashscreen.hide();
-		},1000);
-        app.receivedEvent('deviceready');
+		},5000);
     },
 
     // Update DOM on a Received Event
